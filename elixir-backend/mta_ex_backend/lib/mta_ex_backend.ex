ee5a428b1407b4e22f2e77f72fd7ac5f6cbb80eb
DGSENZEN <diegogaytan2000@gmail.com>
2025-06-08 21:33:51 -0500

    started elixir dev

--- elixir-backend/mta_ex_backend/lib/mta_ex_backend.ex
+++ elixir-backend/mta_ex_backend/lib/mta_ex_backend.ex
@@ -2,17 +2,7 @@ defmodule MtaExBackend do
   @moduledoc """
   Documentation for `MtaExBackend`.
   """
+    use GenServer
+    @gtfs_url "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw"
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MtaExBackend.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
